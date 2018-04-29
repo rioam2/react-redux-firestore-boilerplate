9824592e3c968fae335e3f022c8cb6d02b11b662
zipzapflap <35539750+zipzapflap@users.noreply.github.com>
2018-04-29 02:16:20 -0500

    updated relative path for firestore-subscribe

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import './firebase-redux/firestore-subscribe';
 import store from './firebase-redux/store';
 import App from './App';
-import './firebase-redux/firestore-subscribe';
 import './index.css';
 
 ReactDOM.render(
