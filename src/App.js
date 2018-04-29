330333f9d57732ec5ae07ef20d690823ae0433d0
zipzapflap <35539750+zipzapflap@users.noreply.github.com>
2018-04-29 02:14:09 -0500

    Updated example usage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import authentication from './authentication';
+import auth from './firebase-redux/actions/auth';
+import database from './firebase-redux/actions/database';
 
-const App = props => {
-	const { user } = props; /* eslint-disable-line */
-	return (
-		<div>
-			<h1>Hello, {user.name}</h1>
-			<button onClick={() => authentication.googleAuth()} type="button">
-				Sign in
-			</button>
-		</div>
-	);
+const App = props => (
+	<div>
+		<h1>React-Redux-Firestore-Boilerplate</h1>
+		<h2>
+			{(props.user.isLoggedIn &&
+				`${props.user.data.displayName}, you are logged in`) ||
+				'Please log in.'}
+		</h2>
+		<button onClick={() => auth.googleAuth()} type="button">
+			Sign in
+		</button>
+		<button onClick={() => auth.signOut()} type="button">
+			Sign out
+		</button>
+		<button onClick={() => database.delete('settingsData.test')} type="button">
+			Update Database
+		</button>
+	</div>
+);
+App.propTypes = {
+	user: PropTypes.shape({
+		isLoggedIn: PropTypes.bool.isRequired,
+		data: PropTypes.shape({
+			displayName: PropTypes.string.isRequired
+		})
+	}).isRequired
 };
 
 const mapStateToProps = state => ({
-	user: state.userData,
-	settings: state.settingsData
+	user: state.user,
+	settingsData: state.settingsData
 });
 export default connect(mapStateToProps, null)(App);
